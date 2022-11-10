dde308365008e2d40fa0b5d41c333c3b4820dcde
Zombiekurama <viniciusfventura@gmail.com>
2022-11-10 11:44:59 -0300

    first commit

---
+++ precache-manifest.40d295c48d934ff1406f3aee86e86364.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9221f51f268115103a8",
+    "url": "./static/js/main.a9221f51.chunk.js"
+  },
+  {
+    "revision": "78d5f2475369b964fa39",
+    "url": "./static/css/1.4883df29.chunk.css"
+  },
+  {
+    "revision": "78d5f2475369b964fa39",
+    "url": "./static/js/1.78d5f247.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b014e9fc5defa5d41afa80b7cf287d36",
+    "url": "./index.html"
+  }
+];
---
+++ precache-manifest.4cde7b88f0fbc8e72021b2d986ba02d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a3cd7afdba5d8d30951",
+    "url": "./static/css/main.7e9165aa.chunk.css"
+  },
+  {
+    "revision": "5a3cd7afdba5d8d30951",
+    "url": "./static/js/main.5a3cd7af.chunk.js"
+  },
+  {
+    "revision": "2452cdc0a610ecfd4a67",
+    "url": "./static/js/1.2452cdc0.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "18b02e34fef9f28ab5f2562e45cdffe4",
+    "url": "./index.html"
+  }
+];
